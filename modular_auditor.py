4803aa67e42a89406f65d0e73b755df0c6b4df51
Jun Wei <2602547@sit.singaporetech.edu.sg>
2026-09-18 14:16:44 +0800

    Added process_delivery function

--- modular_auditor.py
+++ modular_auditor.py
@@ -4,4 +4,7 @@ TAX_RATE = 0.10
 def calculate_tax(amount):
     return amount * TAX_RATE
 
-print(calculate_tax(100))
+def process_delivery(current_total, new_value):
+    return current_total + new_value
+
+print(process_delivery(100, 50))
